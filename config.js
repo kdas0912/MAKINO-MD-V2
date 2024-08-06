0d0d95e6d557c8d50bd5be03ef13a86e14a40fc5
kdas0912 <kdas50300@gmail.com>
2024-08-06 15:44:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,26 +12,26 @@ global.antitags = false;
 
 //auto functioner
 global.autoTyping = false;                //make true to enable auto typing
-global.autoRecord = true;                //make true to enable auto recording
+global.autoRecord = false;                //make true to enable auto recording
 global.groupevent = true;                //This is the new variable for controlling group event handling.
 global.statusseen = false;                 //make true to view statuses 
 
 
-global.Owner = [""];
-global.OwnerNumber = [""];
-global.ownertag = [""]; 
-global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
-global.BotName = "🐦Makino-md-v2";
-global.packname = "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";                             //Do not change.
-global.author = "🐦Makino md ᴍᴜʟᴛɪ-ᴅᴇᴠɪᴄᴇ";                               //Do not change.
+global.Owner = ["—♪♪♪ 𝐎𝐆𝐆𝐘••• ♥️🧿🌼"];
+global.OwnerNumber = ["916294147874"];
+global.ownertag = ["—♪♪♪ 𝐎𝐆𝐆𝐘••• ♥️🧿🌼"]; 
+global.OwnerName =  "—♪♪♪ 𝐎𝐆𝐆𝐘••• ♥️🧿🌼";
+global.BotName = "—♪♪♪ 𝐎𝐆𝐆𝐘••• ♥️🧿🌼—MD";
+global.packname = "—♪♪♪ 𝐎𝐆𝐆𝐘••• ♥️🧿🌼";                             //Do not change.
+global.author = "𝐷𝐴𝑅𝐸  𝐷𝐴𝑉𝐼𝐿 ♡⁠‿⁠♡";                               //Do not change.
 global.BotSourceCode = "https://github.com/anonphoenix007/MAKINO-MD-V2"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm "; 
-global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm"; //Do not change!
+global.SupportGroupLink = "https://chat.whatsapp.com/JoyLnRV3KjgEcWeyZ3673D"; 
+global.SupportGroupLink = "https://chat.whatsapp.com/JoyLnRV3KjgEcWeyZ3673D"; //Do not change!
 //global.dbase = process.env.DBASE || "";//Mongodb here
-global.AuthorWhatsapp = "https://wa.me/2347080968564"; //Bot creator WhatsApp link,don't change or bot will not work.
+global.AuthorWhatsapp = "https://wa.me/919832962298"; //Bot creator WhatsApp link,don't change or bot will not work.
 
-global.sessID = process.env.sessID || ``
-global.prefa = ["/"]
+global.sessID = process.env.sessID || `{"noiseKey":{"private":{"type":"Buffer","data":"eGeEc90jIfDTHGnlxfW1dbZOJ5jniNUUND87DUzq20A="},"public":{"type":"Buffer","data":"SoHMEjd7AfJw9c3/xCNZVnlHD12H2Ayy3NBCMKqWqDA="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"sHHtEk8JQrK6BczGBvz8KLhocJoIqVO1oTK6WWra4V8="},"public":{"type":"Buffer","data":"R50/n9Si5sqp77wKZhdEg5t5CCeOGHYQmuwoJMDiCiA="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"gLFINKXczoD0cH4o2biW1lrj3rWMb/NrKh38F22PPkg="},"public":{"type":"Buffer","data":"xPFdPHOkO5eaIK0TEdBPAyK6+guVdPuDFdVUcRvvMwU="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"UKtAO6Tx4PsuDjusltRLfXWXON2luCIYvZ6Pb1Sb+lc="},"public":{"type":"Buffer","data":"PPf1KKwqASYvuWP02FTZY2g9lrSQsEEmXTNDhqSc8Rc="}},"signature":{"type":"Buffer","data":"smbwXwLzNKD94MncEtBUqF1wjXzy/gy4IT9vgevibrfLNf3b+XNXi1WXAmFvtwzNasyoSI4h8igcekZOjsk5gg=="},"keyId":1},"registrationId":75,"advSecretKey":"ahmwiindOVKRGRm4Gzri1TPqbWp8yHaH+3FR2aw76MM=","processedHistoryMessages":[],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":0,"accountSettings":{"unarchiveChats":false},"deviceId":"uxyhIaZtSFKXLBgTdHCQRA","phoneId":"48622c86-bf7a-474e-b5b6-dd4afabd6a37","identityId":{"type":"Buffer","data":"Mw3FHfsI3+8W9i9/V8cA9oeh16c="},"registered":true,"backupToken":{"type":"Buffer","data":"UN6px6dFyPygH1OZQ7La+eU8ttI="},"registration":{},"pairingCode":"KV89CQ4S","me":{"id":"916294147874:44@s.whatsapp.net"},"account":{"details":"CJOV1/cFEKnfx7UGGAMgACgA","accountSignatureKey":"Qgx4B9SvRGW1R64EdzW7hC8ZczRKtoN5g0ui5ISCThA=","accountSignature":"66ImLGzSvhI6iDfeuE6QmMmsj+pQm35CSv8Dm+WJLDT6Z5uVjjczqtyFhWtzuxBtbMjupdOZoYEAACE8BlP0DA==","deviceSignature":"o/xsX79DiWdx3MlscPoFdnmjPQ4MBOPKDqKR+LXv9hCMDxNb3VVIVrEAYSFOU75pMmF7Sn3eyThc7StTKSoriQ=="},"signalIdentities":[{"identifier":{"name":"916294147874:44@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BUIMeAfUr0RltUeuBHc1u4QvGXM0SraDeYNLouSEgk4Q"}}],"platform":"android","lastAccountSyncTimestamp":1722937270,"myAppStateKeyId":"AAAAADRO"}`
+global.prefa = ["."]
 
 //
 global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
